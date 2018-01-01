e3ccc0dd06d838d966adb1e0be57114e4f451b06
Jake Tran <jaketran33@gmail.com>
2017-12-31 18:15:09 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Wallpaper
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.UrlBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(639, 505);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(132, 89);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Change!";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(604, 269);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(202, 29);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Enter image URL:";
+			// 
+			// UrlBox
+			// 
+			this.UrlBox.Location = new System.Drawing.Point(328, 362);
+			this.UrlBox.Name = "UrlBox";
+			this.UrlBox.Size = new System.Drawing.Size(755, 35);
+			this.UrlBox.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1400, 1127);
+			this.Controls.Add(this.UrlBox);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox UrlBox;
+	}
+}
+
